16f0a6697598305d3ddfdd7fdd5803785c1a96ea
María del Pilar Alegre Ramos <malegrera@uoc.edu>
2022-04-03 21:36:58 +0200

    cambios para desarrollo1

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -4,8 +4,8 @@ import {nombre, i as ranas, fecha} from "./otro";
 const mensaje:string = `<p>Hola ${nombre}</p>`;
 const mensaje2:string = "<p>Hello World again!</p>";
 
-document.querySelector("main").insertAdjacentHTML('beforeend', mensaje);
-document.querySelector("main").insertAdjacentHTML('beforeend', mensaje2);
+document.querySelector("div").insertAdjacentHTML('beforeend', mensaje);
+document.querySelector("div").insertAdjacentHTML('beforeend', mensaje2);
 console.log(ranas);
 let p:HTMLElement=document.querySelector("p:nth-of-type(2)");
 p.className="fa-solid";
